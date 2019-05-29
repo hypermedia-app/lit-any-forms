f97f721e7fee5f12f5c5217bed778b9dd6aa3b51
tpluscode <tpluscode@users.noreply.github.com>
2019-05-29 13:24:47 +0200

    fix build

--- lib/TemplateRegistry.ts
+++ lib/TemplateRegistry.ts
@@ -2,18 +2,18 @@ import TemplateRegistry from '@lit-any/core/template-registry'
 import fallbackComponents from './FallbackComponents'
 import TemplateSelectorBuilder from './TemplateSelectorBuilder'
 import { Criteria, RenderFunc } from './index'
-import { ButtonComponent, Index } from './components/ComponentSet'
+import { ButtonComponent, ComponentSet } from './components'
 
 // eslint-disable-next-line max-len
 export default class FormTemplateRegistry extends TemplateRegistry<TemplateSelectorBuilder, Criteria, RenderFunc> {
-    public components: Index & ButtonComponent
+    public components: ComponentSet & ButtonComponent
 
     public constructor(name: string) {
         super(name)
         this.components = fallbackComponents
     }
 
-    public useComponents(components: Index) {
+    public useComponents(components: ComponentSet) {
         this.components = {
             ...fallbackComponents,
             ...components,
